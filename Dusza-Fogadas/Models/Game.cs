093b842646dd796259cac68d9de08af5b00a653a
Györfi Marcell <gyorfimarcell@gmail.com>
2024-09-26 14:02:16 +0200

    WIP

---
+++ Dusza-Fogadas/Models/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dusza_Fogadas.Models
+{
+    public class Game(int id, string name, int organizerId)
+    {
+        public static List<Game> Games = [];
+
+        public int Id { get; private set; } = id;
+        public string Name { get; private set; } = name;
+        public int OrganizerId { get; private set; } = organizerId;
+
+        public List<GameSubject> Subjects => GameDb.GetSubjects(Id);
+        public List<GameEvent> Events => GameDb.GetEvents(Id);
+
+        public User Organizer => User.Users.Find(x => x.Id == OrganizerId);
+
+        public static Game NewGame(string name, List<string> subjects, List<string> events)
+        {   
+            
+        }
+    }
+}
---
+++ Dusza-Fogadas/Models/GameEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dusza_Fogadas.Models
+{
+    public class GameEvent(int id, int gameId, string name)
+    {
+        public int Id { get; private set; } = id;
+        public int GameId { get; private set; } = gameId;
+        public string Name { get; private set; } = name;
+
+        public Game Game => Game.Games.Find(x => x.Id == GameId);
+    }
+}
