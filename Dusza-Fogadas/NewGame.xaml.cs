959515f023f763d9005fdb9169e4868372e0e939
dkrisztian2 <20.dobos.krisztian.attila@mechwart.com>
2024-09-26 12:39:08 +0200

    Játék létrehozása ablakot elkezdtem

---
+++ Dusza-Fogadas/NewGame.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dusza_Fogadas
+{
+    /// <summary>
+    /// Interaction logic for NewGame.xaml
+    /// </summary>
+    public partial class NewGame : Window
+    {
+        public NewGame()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBackToMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow newLoginWindow = new MainWindow();
+            this.Close();
+            newLoginWindow.ShowDialog();
+        }
+    }
+}
